8bd41df632d8b2acb5efe1f1943bd6d4f215edb1
Elmar Ismayilov <bamazy@gmail.com>
2017-09-27 17:04:40 +0400

    changes

---
+++ src/AppBundle/Controller/GroupController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Doctrine\ORM\EntityManagerInterface;
+use AppBundle\Entity\Groups;
+use AppBundle\Form\UserType;
+
+class GroupController extends Controller
+{
+    /**
+    * @Route("/group", name="group_list")
+    */
+    public function listAction()
+    {
+        // find all users
+        $groups = $this->getDoctrine()->getRepository(Groups::class)->findAll();
+
+        return $this->render('admin/index.html.twig', array(
+            'groups' => $groups
+        ));
+    }
+
+    /**
+    * @Route("/group/add", name="group_add")
+    */
+    public function addAction()
+    {
+        $group = new Groups();
+
+        $form = $this->createForm(GroupType::class, $group);
+
+        // $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $task = $form->getData();
+    
+            return $this->redirectToRoute('group_list');
+        }
+
+        return $this->render('admin/form.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+}
---
+++ src/AppBundle/Form/GroupType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class GroupType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('save', SubmitType::class, array('label' => 'Add Group'))
+        ;
+    }
+}
