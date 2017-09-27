a1dfa884de68a2f53de72fce9282070fdcd69cf8
Elmar Ismayilov <bamazy@gmail.com>
2017-09-27 18:46:10 +0400

    changes

--- src/AppBundle/Entity/Users.php
+++ src/AppBundle/Entity/Users.php
@@ -29,6 +29,16 @@ class Users
      */
     private $group;
 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
     public function getName()
     {
         return $this->name;
