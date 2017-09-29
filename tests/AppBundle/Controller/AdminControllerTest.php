e05098d6d625fc483e15d216f3f437a23b9cfcca
Elmar Ismayilov <bamazy@gmail.com>
2017-09-29 16:11:57 +0400

    added new test rule

--- tests/AppBundle/Controller/AdminControllerTest.php
+++ tests/AppBundle/Controller/AdminControllerTest.php
@@ -15,4 +15,19 @@ class AdminControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Sample Admin page!', $crawler->filter('.container h1')->text());
     }
+
+    public function testAddUser()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/user/add');
+
+        $form = $crawler->selectButton('submit')->form();
+
+        $form['name'] = 'Lucas';
+
+        $crawler = $client->submit($form);
+
+        $this->assertContains('Lucas', $crawler->filter('table > tr > td > a')->text());
+    }
 }
