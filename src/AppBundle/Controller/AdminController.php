1692b4b7584cc1d4bbca1220ebb90afe135cbdfc
Elmar Ismayilov <bamazy@gmail.com>
2017-10-01 17:07:08 +0400

    changes

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -99,7 +99,7 @@ class AdminController extends Controller
                 // $em->flush();
         
                 $response = new Response();
-                $jsonContent = $serializer->serialize($formData['name'], 'json');
+                $jsonContent = $serializer->serialize($formData, 'json');
                 $response->setContent($jsonContent);
                 $response->headers->set('Content-Type', 'application/json');
                 $response->setStatusCode(Response::HTTP_OK);
