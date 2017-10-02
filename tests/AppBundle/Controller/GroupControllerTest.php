0e0ab9329897ab679c8e2600ff92dc629e2b947f
Elmar Ismayilov <bamazy@gmail.com>
2017-10-02 18:13:53 +0400

    changes

--- tests/AppBundle/Controller/GroupControllerTest.php
+++ tests/AppBundle/Controller/GroupControllerTest.php
@@ -13,7 +13,21 @@ class GroupControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/group');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        // $this->assertContains('Welcome to Sample Admin page!', $crawler->filter('a:contains("Add a group")')->text());
+    }
+
+    public function testAddUser()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/group/add');
+
+        $form = $crawler->selectButton('Save Group')->form();
+
+        $form['group[name]'] = 'Developers';
+
+        $client->submit($form);
+
+        $this->assertEquals('AppBundle\Controller\GroupController::addAction', $client->getRequest()->attributes->get('_controller'));
     }
 
 }
